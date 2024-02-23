3c9e51b556c49b6f944d78065e72e8a1df7bfc1b
PranayChavhan <pranay21chavhan@outlook.com>
2024-02-23 14:43:57 +0530

    welcome screen dots

--- screens/OnBoarding/WelcomeScreen.js
+++ screens/OnBoarding/WelcomeScreen.js
@@ -5,6 +5,7 @@ import {
   Image,
   TouchableOpacity,
   ImageBackground,
+  TouchableWithoutFeedback
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
@@ -41,6 +42,11 @@ export default function WelcomeScreen() {
     }
   };
 
+  const handleSkip = () => {
+    setCurrentPage(3);
+  };
+
+
   return (
     <SafeAreaView className="">
       <View className="p-4">
@@ -51,6 +57,15 @@ export default function WelcomeScreen() {
         </View>
 
         <View className=" h-full flex flex-col justify-center gap-4">
+
+        <View className="absolute right-0 top-10">
+            <TouchableWithoutFeedback onPress={handleSkip}>
+              <Text className={`text-[14px] font-medium text-[#2F80ED] underline text-lg`}>Skip</Text>
+            </TouchableWithoutFeedback>
+          </View>
+
+
+
           <ImageBackground
             source={require("../../assets/images/HeroVector.png")}
             style={{ width: 370, height: 300 }}
@@ -66,9 +81,13 @@ export default function WelcomeScreen() {
               {pages[currentPage - 1].description}
             </Text>
 
-            <View className="flex flex-row">
+            <View className="flex flex-row gap-1 items-center justify-center">
               {pages.map((_, index) => (
-                <Text key={index}>{currentPage === index + 1 ? "●" : "○"}</Text>
+                <Image 
+                key={index} 
+                source={currentPage === index + 1 ? require('../../assets/icons/rdot.png') : require('../../assets/icons/dot.png')} 
+                // style={{ width: 20, height: 20 }} 
+              />
               ))}
             </View>
           </View>
